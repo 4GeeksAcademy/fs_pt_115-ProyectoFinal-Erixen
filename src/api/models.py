c1597728ec8ea33532338696a67c6bf9f0869333
Carlos Enrique Contreras Duque <carlos.contredu@gmail.com>
2025-08-29 17:14:09 +0000

    proceso de backend

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,119 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeingKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship 
 
 db = SQLAlchemy()
 
+#------------------------------------------------------------------------------------------------
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    name: Mapped[str] = mapped_column(String(30), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(30), nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    telefono: Mapped[int] = mapped_column(int(20), nullable=False)
+    rol: Mapped[list]
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    reservas_id: Mapped[list["Reserva"]] = relationship(backpopulates="user_reserva")
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "lastname": self.lastname,
+            "telefono": self.telefono,
             "email": self.email,
+            "rol": self.rol,
+            "reservas_id": self.reservas_id
             # do not serialize the password, its a security breach
         }
+
+#------------------------------------------------------------------------------------------------
+class Club(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30), nullable=False)
+    cif_nif: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
+    direccion: Mapped[str] = mapped_column(String(255), nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    telefono: Mapped[int] = mapped_column(int(20), nullable=False)
+    horario_apertura_cierre: Mapped[str] = mapped_column(String(255))
+    num_pista: Mapped[list["Pista"]] = relationship(backpopulates="pista")
+    # password: Mapped[str] = mapped_column(nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "cif_nif": self.cif_nif,
+            "direccion": self.lastname,
+            "telefono": self.telefono,
+            "email": self.email,
+            "horario_apertura_cierre": self.horario_apertura_cierre,
+            "num_pista": self.num_pista
+            # do not serialize the password, its a security breach
+        }
+
+#------------------------------------------------------------------------------------------------
+class Pista(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    id_club: Mapped[int] = relationship(ForeingKey("club_id"))
+    numero_pista: Mapped[int]
+    superficie: Mapped[list]
+    precio_hora: Mapped[float]
+    estado: Mapped[list]
+    pista:Mapped["Club"] = relationship(backpopulates="num_pista")
+    reserva_pista: Mapped[list["Reserva"]] = relationship(backpopulates="pista_reservada")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "numero_pista": self.numero_pista,
+            "superficie": self.superficie,
+            "precio_hora": self.precio_hora,
+            "estado": self.estado
+        }
+
+#------------------------------------------------------------------------------------------------
+class Reserva(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    id_pista: Mapped[int]
+    fecha_reserva: Mapped[str] #aqui va un dato de tiempo
+    hora_inicio: Mapped[str]
+    hora_fin: Mapped[str] 
+    precio_total: Mapped[float]
+    estado: Mapped[enumerate]
+    user_reserva: Mapped[int] = relationship(backpopulates="reserva_id")
+    pista_reservada: Mapped["Pista"] = relationship(backpopulates="reserva_pista")
+    pago_id: Mapped["Pago"] = relationship(backpopulates="reserva_pago")
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "id_pista": self.id_pista,
+            "fecha_reserva": self.fecha_reserva,
+            "hora_inicio": self.hora_inicio,
+            "hora_fin": self.hora_fin,
+            "precio_total": self.precio_total,
+            "estado": self.estado
+        }
+    
+#------------------------------------------------------------------------------------------------    
+class Pago(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    id_reserva: Mapped[int] = relationship(ForeingKey("id_reserva"))  
+    metodo_pago: Mapped[list]
+    monto_cantidad: Mapped[float]
+    estado_pago: Mapped[list] 
+    pago_reserva: Mapped["Reserva"] = relationship(backpopulates="pago_id")
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "id_reserva": self.id_reserva,
+            "metodo_pago": self.metodo_pago,
+            "monto_cantidad": self.monto_cantidad,
+            "estado_pago": self.estado_pago
+        }
+    
+
+#------------------------------------------------------------------------------------------------    
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,9 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from models import User, Club, Pista, Reserva, Pago
+
+#------------------------------------------------------------------------------------------------
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +23,88 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+#------------------------------------------------------------------------------------------------
+
+@api.routes('/users', methods=['GET'])
+def get_all_users():
+    users= User.query.all()
+    if not users:
+        return jsonify({"msg": "ERROR"}), 404
+    return jsonify([user.serialize() for user in users]), 200  
+
+@api.routes('/users/<int:id>', methods=['GET'])
+def get_user(id):
+    unique_user= User.query.get(id)
+    if not unique_user:
+        return ({"message": "User not found, try with other user."}), 404
+    return jsonify([user.serialize()]), 200
+
+@api.routes('/users', methods=['POST'])
+def create_user():
+    data= request.get_json()
+    if not data.get("name") or not data.get("email") or not data.get("password"):
+        return jsonify({"message": "Name, Email and Password are required"})
+    
+    new_user = User(
+
+        name = data["name"],
+        email = data["email"],
+        password = data["password"]
+    )
+    
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify(new_user.serialize()), 201
+#------------------------------------------------------------------------------------------------
+
+@api.routes('/clubs', methods=['GET'])
+def get_all_clubs():
+    clubs= Club.query.all()
+    if not clubs:
+        return jsonify({"message": "ERROR"}), 404
+    return jsonify([club.serialize() for club in clubs]), 200
+
+
+@api.routes('/clubs/<int:id>', methods=['GET'])
+def get_club(id):
+    unique_club= Club.query.get(id)
+    if not unique_club:
+        return({"message": "Club not found, try with other club."}), 404
+    return jsonify([club.serialize()]), 200
+
+@api.routes('/clubs', methods=['POST'])
+def create_club():
+    data= request.get_json()
+    if not data("email") or not data("password") or not data("cif_nif"):
+        return jsonify({"message": "Email, Password and CIF/NIF are required."}), 404
+    
+    new_club= Club(
+
+        email=data["email"],
+        password=data["password"],
+        cif=data["cif_nif"]
+    )
+
+    db.session.add(new_club)
+    db.session.commit()
+    return jsonify(new_club.serialize())
+#------------------------------------------------------------------------------------------------
+
+@api.routes('/pistas', methods=['GET'])
+def get_all_pistas():
+    pistas= Pista.query.all()
+    if not pistas:
+        return jsonify({"message": "ERROR"}), 404
+    return jsonify([pista.serialize() for pista in pistas]), 200
+
+@api.routes('/pistas/<int:id>', methods=['GET'])
+def get_pista(id):
+    unique_pista= Pista.query.get(id)
+    if not unique_pista:
+        return jsonify({"message": "Pista not found, try with other pista."})
+    return jsonify([pista.serialize()]), 200
+
+#------------------------------------------------------------------------------------------------
+
