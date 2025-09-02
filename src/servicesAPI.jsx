6fc6a71b067733656ce0da5b72849a3337284eaf
Carlos Enrique Contreras Duque <carlos.contredu@gmail.com>
2025-09-02 17:10:08 +0000

    llamadas en progress

--- src/servicesAPI.jsx
+++ src/servicesAPI.jsx
@@ -25,7 +25,7 @@ export const ServicesAPI = () => {
 	// USERS
 
 	const getUsers = async () => {
-		const response = await fetch("http://localhost:3001/api/users");
+		const response = await fetch("https://silver-rotary-phone-wv5697xw6vc9xpg-3001.app.github.dev/api/users");
 		const data = await response.json();
 		setusers(data);
 		return data;
@@ -34,7 +34,7 @@ export const ServicesAPI = () => {
 	}
 
 	const getUser = async (id) => {
-		const response = await fetch(`http://localhost:3001/api/users/${id}`);
+		const response = await fetch(`https://silver-rotary-phone-wv5697xw6vc9xpg-3001.app.github.dev/api/users/${id}`);
 		
 		if(!response.ok){
 			console.log("There's not user, create one");
@@ -47,7 +47,7 @@ export const ServicesAPI = () => {
 	}
 
     const createUser = async () => {
-        const response = await fetch("http://localhost:3001/api/users", {
+        const response = await fetch("https://silver-rotary-phone-wv5697xw6vc9xpg-3001.app.github.dev/api/users", {
             method: "POST",
 			headers: {
 				"Content-Type": "application/json"
@@ -66,14 +66,14 @@ export const ServicesAPI = () => {
 	//---------------------------------------------------------------------
 	// CLUBS
 	const getclubs = async () => {
-		const response = await fetch("http://localhost:3001/api/clubs");
+		const response = await fetch("https://silver-rotary-phone-wv5697xw6vc9xpg-3001.app.github.dev/api/clubs");
 		const data = await response.json();
 		setClubs(data);
 		return data;
 	}
 
 	const getclub = async (id) => {
-		const response = await fetch(`http://localhost:3001/api/clubs/${id}`);
+		const response = await fetch(`https://silver-rotary-phone-wv5697xw6vc9xpg-3001.app.github.dev/api/clubs/${id}`);
 		
 		if(!response.ok){
 			console.log("There's not club, create one");
@@ -86,7 +86,7 @@ export const ServicesAPI = () => {
 	}
 	
 	const createclub = async () => {
-		const response = await fetch("http://localhost:3001/api/clubs", {
+		const response = await fetch("https://silver-rotary-phone-wv5697xw6vc9xpg-3001.app.github.dev/api/clubs", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json"
@@ -104,14 +104,14 @@ export const ServicesAPI = () => {
     //---------------------------------------------------------------------
 	// PISTAS
 	const getpistas = async () => {
-		const response = await fetch("http://localhost:3001/api/pistas");
+		const response = await fetch("https://silver-rotary-phone-wv5697xw6vc9xpg-3001.app.github.dev/api/pistas");
 		const data = await response.json();
 		setPistas(data);
 		return data;
 	}
 
 	const getpista = async (id) => {
-		const response = await fetch(`http://localhost:3001/api/pistas/${id}`);
+		const response = await fetch(`https://silver-rotary-phone-wv5697xw6vc9xpg-3001.app.github.dev/api/pistas/${id}`);
 		
 		if(!response.ok){
 			console.log("There's not pista, look for another.");
@@ -127,14 +127,14 @@ export const ServicesAPI = () => {
 	//---------------------------------------------------------------------
 	// RESERVAS
 	const getreservas = async () => {
-		const response = await fetch("http://localhost:3001/api/reservas");
+		const response = await fetch("https://silver-rotary-phone-wv5697xw6vc9xpg-3001.app.github.dev/api/reservas");
 		const data = await response.json();
 		setreservas(data);
 		return data;
 	}
 
 	const getreserva = async (id) => {
-		const response = await fetch(`http://localhost:3001/api/reservas/${id}`);
+		const response = await fetch(`https://silver-rotary-phone-wv5697xw6vc9xpg-3001.app.github.dev/api/reservas/${id}`);
 		
 		if(!response.ok){
 			console.log("There's not reserva, look for another.");
@@ -147,10 +147,42 @@ export const ServicesAPI = () => {
 
 	}
 
+	const createReserva = async () => {
+		const response = await fetch("https://silver-rotary-phone-wv5697xw6vc9xpg-3001.app.github.dev/api/reservas", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				fecha: "2024-06-30",
+				hora: "10:00",
+				userId: 1,
+				pistaId: 1
+			})
+		})
+		const data = await response.json();
+		setreserva((prev) => [...prev, data]);
+		return data;		
+	}
+
 	//---------------------------------------------------------------------
 	
 	
-	return ({
+	return (
 		<>
+			<h1>Servicios API</h1>
+
+			<button onClick={getUsers}>Get Users</button>
+			<button onClick={() => getUser(1)}>Get User 1</button>
+			<button onClick={() => createUser}>Registrarse</button>
+			<button onClick={getClubs}>Get Clubs</button>
+			<button onClick={() => getClub(1)}>Get Club 1</button>
+			<button onClick={() => createclub}>Registro Club</button>
+			<button onClick={getPistas}>Get Pistas</button>
+			<button onClick={()=> getpista(1)}>Get Pistas</button>
+			<button onClick={getreservas}>Get Reservas</button>
+			<button onClick={()=> getreserva(1)}>Get Pistas</button>
+			<button onClick={() => createReserva}>Reservar</button>
 		</>
-	});
+	)
+}
