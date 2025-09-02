32dd885feaeba1d4445089b8b31d342a038860aa
Carlos Enrique Contreras Duque <carlos.contredu@gmail.com>
2025-09-02 16:12:34 +0000

    creacion de funciones

--- src/servicesAPI.jsx
+++ src/servicesAPI.jsx
@@ -1,24 +1,49 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+
 export const ServicesAPI = () => {
 
-    
+	//---------------------------------------------------------------------
+	// VAMOS A CREAR LOS STATES PARA GUARDAR LA INFORMACION DE LA API
+	
+	const [users, setusers] = useState([]);
+	const [user, setuser] = useState(null);
+
+	const [clubs, setClubs] = useState([]);
+	const [club, setClub] = useState(null);
+
+	const [pistas, setPistas] = useState([]);	
+
+	const [reservas, setreservas] = useState([]);
+	const [reserva, setreserva] = useState(null);
+	
+
+
+
+    //---------------------------------------------------------------------
+	// VAMOS A CREAR LAS FUNCIONES PARA HACER PETICIONES A LA API
+	// USERS
+
 	const getUsers = async () => {
 		const response = await fetch("http://localhost:3001/api/users");
 		const data = await response.json();
-		console.log(data);
+		setusers(data);
+		return data;
 		
-		// me falta crear la funcion de seteo del usuarios UseState
+		
 	}
 
 	const getUser = async (id) => {
 		const response = await fetch(`http://localhost:3001/api/users/${id}`);
+		
 		if(!response.ok){
 			console.log("There's not user, create one");
-			return
+			return null;
 		}
 
 		const data = await response.json();
-		// me falta crear la funcion de seteo del usuario UseState}
-		console.log(data);
+		setuser(data);
+		return data;
 	}
 
     const createUser = async () => {
@@ -33,26 +58,31 @@ export const ServicesAPI = () => {
 				password: "New Password"
 			})
         })
+		const data = await response.json();
+		setuser((prev) => [...prev, data]);
+		return data;
     }    
 
 	//---------------------------------------------------------------------
-
+	// CLUBS
 	const getclubs = async () => {
 		const response = await fetch("http://localhost:3001/api/clubs");
 		const data = await response.json();
-		
+		setClubs(data);
+		return data;
 	}
 
 	const getclub = async (id) => {
 		const response = await fetch(`http://localhost:3001/api/clubs/${id}`);
+		
 		if(!response.ok){
 			console.log("There's not club, create one");
-			return
+			return null;
 		}
 
 		const data = await response.json();
-		console.log(data);
-		
+		setClub(data);
+		return data;
 	}
 	
 	const createclub = async () => {
@@ -68,13 +98,59 @@ export const ServicesAPI = () => {
 			})
 		})
 		const data = await response.json();
-		console.log(data);
-		
+		setClub((prev) => [...prev, data]);
+		return data;		
 	}
     //---------------------------------------------------------------------
-    return (
-        
-        <div className="container"></div>
+	// PISTAS
+	const getpistas = async () => {
+		const response = await fetch("http://localhost:3001/api/pistas");
+		const data = await response.json();
+		setPistas(data);
+		return data;
+	}
 
-    );
-}
+	const getpista = async (id) => {
+		const response = await fetch(`http://localhost:3001/api/pistas/${id}`);
+		
+		if(!response.ok){
+			console.log("There's not pista, look for another.");
+			return null;
+		}
+
+		const data = await response.json();
+		setPistas(data);
+		return data;
+
+	}
+    
+	//---------------------------------------------------------------------
+	// RESERVAS
+	const getreservas = async () => {
+		const response = await fetch("http://localhost:3001/api/reservas");
+		const data = await response.json();
+		setreservas(data);
+		return data;
+	}
+
+	const getreserva = async (id) => {
+		const response = await fetch(`http://localhost:3001/api/reservas/${id}`);
+		
+		if(!response.ok){
+			console.log("There's not reserva, look for another.");
+			return null;
+		}
+
+		const data = await response.json();
+		setreserva(data);
+		return data;
+
+	}
+
+	//---------------------------------------------------------------------
+	
+	
+	return ({
+		<>
+		</>
+	});
