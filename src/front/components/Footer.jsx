8668babb7117d801d05ccea9b77a9393a7a14873
mariogr12 <mariogrguitar12@gmail.com>
2025-09-10 18:46:22 +0000

    diseño del footer

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,16 +1,37 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 export const Footer = () => {
 
-	return(
+	return (
+		<div>
 
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Check the <a target="_blank" href="https://4geeks.com/docs/start/react-flask-template">template documentation</a> <i className="fa-solid fa-file"></i> for help.
-		</p>
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+			<div className="div d-flex">
+				<div className="footer mt-auto py-3 bg-light d-flex justify-content-evenly w-50 flex-column">
+					<div className="btn btn-success myButton bg-transparent border-0 text-black">Declaración de privacidad</div>
+					<div className="btn btn-success myButton bg-transparent border-0 text-black">Información sobre cuentas</div>
+					<div className="btn btn-success myButton bg-transparent border-0 text-black">Condiciones de uso</div>
+				</div>
+				<div className="bg-light w-50 py-3 ps-2">
+					<div className='mx-auto'>
+						<p className="text-black ps-1 mb-0" style={{ fontSize: "20px" }}>Síguenos en nuestras redes sociales!</p>
+						<div className='d-flex'>
+							<btn className="btn btn-primary m-1 text-black p-0 me-2 bg-transparent border-0" style={{ fontSize: "40px" }}>
+								<i class="bi bi-instagram"></i>
+							</btn>
+							<btn className="btn btn-primary m-1 text-black p-0 me-2 bg-transparent border-0" style={{ fontSize: "40px" }}>
+								<i class="bi bi-twitter-x"></i>
+							</btn>
+							<btn className="btn btn-primary m-1 text-black p-0 m-0 bg-transparent border-0" style={{ fontSize: "40px" }}>
+								<i class="bi bi-facebook"></i>
+							</btn>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='bg-dark text-white text-center py-2'>
+				Padel+ S.L 2018 <i class="bi bi-c-circle"></i>  Todos los derechos reservados
+			</div>
+		</div>
 	)
 };
 
