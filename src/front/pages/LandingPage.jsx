20b8d9c71a8b99e5bb901360fe7c271099076a70
Jhon Cordova <jhon.a.cordova.martinez@gmail.com>
2025-09-13 05:12:52 +0000

    agregado de images, modificaciones en home y LandingPage

--- src/front/pages/LandingPage.jsx
+++ src/front/pages/LandingPage.jsx
@@ -50,7 +50,7 @@ export const LandingPage = () => {
 
 				<main className="main-content">
 					{/* Formas para el fondo. */}
-					{/* Los elementos del fondo animado se han eliminado de aqui, ahora estan en layout */}
+					{/* Los elementos del fondo animado se han eliminado de aqui, ahora estan en layout con la finalidad de que sean globales;(PENDIENTE)*/}
 
 					<div className="hero-content">
 						{/* Contenido principal de la landing page. */}
@@ -61,9 +61,12 @@ export const LandingPage = () => {
 							Conecta con jugadores de tu nivel, organiza partidos y mejora tu juego. Tu próximo partido está a un solo clic de distancia.
 						</p>
 						<div style={{ marginTop: '2rem' }}>
-							<button className="btn btn-primary btn-lg">
-								Buscar Partido
-							</button>
+							{/* Botón para buscar un partido. redirecciona a home*/}
+							<a href="/home" target="_blank" rel="noopener noreferrer">
+								<button className="btn btn-primary btn-lg">
+									Buscar Partido
+								</button>
+							</a>
 						</div>
 					</div>
 				</main>
