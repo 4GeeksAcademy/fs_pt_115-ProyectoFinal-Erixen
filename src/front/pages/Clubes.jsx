c1b86f8277dc3d5b2d0b52e87925f50395271ee7
Carlos Enrique Contreras Duque <carlos.contredu@gmail.com>
2025-09-19 18:43:55 +0000

    formulario para crear club

--- src/front/pages/Clubes.jsx
+++ src/front/pages/Clubes.jsx
@@ -8,7 +8,8 @@ export const Clubes = () => {
 
 	const navigate = useNavigate()
 	const [clubs, setClubs] = useState([]);
-  
+	
+
 	const getClubsFromApi = async () => {
 		const clubsApi = await getClubs()
 		setClubs(clubsApi)
@@ -21,28 +22,32 @@ export const Clubes = () => {
 	}, [localStorage.getItem("token")]);
 
 
-	//FUNCION PARA UN FUTURO PARA CONSEGUIR LAS PISTAS
-	
-	// useEffect(()=>{
-	// 	getClubsFromApi()
-	// 		
-	// },[])
+	// FUNCION PARA UN FUTURO PARA CONSEGUIR LAS PISTAS
+
+	useEffect(()=>{
+		getClubsFromApi()
+			
+	},[])
 
 	return (
 		<>
-			<div className="container">
+			<PageHeader
+				title="Clubes"
+				lead="Encuentra tu club ideal"
+			/>
+			<div className="container row mx-auto">
 				{/*carousel*/}
 				<div className="card mt-2" style={{ width: "400px", height: "700px" }}>
 					<div id="carouselExample" className="carousel slide">
 						<div className="carousel-inner">
 							<div className="carousel-item active ratio ratio-1x1">
-								<img src="src/front/assets/1.webp" className="d-block w-100" alt="nombre del polideportivo" />
+								<img src="src/front/assets/1.webp" className="d-block w-100" style={{ objectPosition: "center", objectFit: "cover" }} alt="nombre del polideportivo" />
 							</div>
 							<div className="carousel-item ratio ratio-1x1">
-								<img src="src/front/assets/2.webp" className="d-block w-100" alt="nombre del polideportivo" />
+								<img src="src/front/assets/2.webp" className="d-block w-100" style={{ objectPosition: "center", objectFit: "cover" }} alt="nombre del polideportivo" />
 							</div>
 							<div className="carousel-item ratio ratio-1x1">
-								<img src="src/front/assets/3.webp" className="d-block w-100" alt="nombre del polideportivo" />
+								<img src="src/front/assets/3.webp" className="d-block w-100" style={{ objectPosition: "center", objectFit: "cover" }} alt="nombre del polideportivo" />
 							</div>
 						</div>
 						<button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
@@ -60,16 +65,49 @@ export const Clubes = () => {
 							y eligiendo jugar en la pista que mas te guste, si quieres consultar todas nuestras pistas haz click en el siguiente boton.</p>
 					</div>
 					<button className="btn btn-primary m-1"> Ver pistas </button>
-					
-					{/* esto va a ser la seccion donde se desplegara el boton de ver pistas EN PROCESO NO TOCAR POR EL MOMENTO */}
-					<div className="conteiner-fluid border">
-						<div className="img">
-							<img src=""/>
-						</div>
-					</div>
-
 				</div>
 			</div>
+				{/* esto va a ser la seccion donde se desplegara el boton de ver pistas EN PROCESO NO TOCAR POR EL MOMENTO */}
+			<div className="container-fluid border text-primary d-flex row p-2 m-1">
+					<form>
+						<div className="Nombre del polideportivo row m-3">
+							<label className="mb-1" for="Name" placeholder="Nombre del club">Nombre del polideportivo</label>
+							<input type="text" id="Name"></input>
+						</div>
+						<div className="imagenes row m-3">
+							<label for="imagenes" className="mb-3">Inserta imagenes del club</label>
+							<input type="file" id="imagenes" placeholder="Obligatoria"></input>
+							<input type="file" id="imagenes" placeholder="Opcional"></input>
+							<input type="file" id="imagenes" placeholder="Opcional"></input>
+						</div>
+						<div className="description row m-3">
+							<label className="mb-1" for="description">Descripcion del club</label>
+							<input type="text" id="description" placeholder="Descripcion del club"></input>
+						</div>
+					</form>
+
+				</div>
+
+				<div className="container justify-content-center align-item-center d-flex mx-auto">
+					<button className="btn btn-primary">Crear Club.</button>
+			</div>
+			
+
+
 		</>
 	)
 };
+
+
+// faltantes:
+
+// -en el modals de club necesito mi parametro de imagenes 
+// -parametro de descripcion
+
+
+
+
+// Idea de jhon en vez de integrar el boton de crear club clásico la idea es tener esa opción pero en una forma de 
+// tarjeta con el link que te lleve a crear el club y una vez que se cree el club que siga apareciendo la opcion de crear 
+// otro club.
+
