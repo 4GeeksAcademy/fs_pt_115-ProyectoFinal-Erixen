ebd0b8af241c7314acc92758787b57d93c8c4d83
Alexisca <150233709+Thealexisca22@users.noreply.github.com>
2025-09-23 08:37:53 +0000

    Guardar cambios locales antes del merge

--- src/front/pages/ReservasUsuario.jsx
+++ src/front/pages/ReservasUsuario.jsx
@@ -2,9 +2,6 @@ import { PageHeader } from "../components/PageHeader"; // 1. Importar
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { getPista, getReserva, getUser } from "../../services/servicesAPI";
-
-
 
 export const ReservasUsuario = () => {
 const [reserva, setreserva] = useState();
@@ -17,66 +14,93 @@ const [reserva, setreserva] = useState();
 		}
 	}, [localStorage.getItem("token")]);
 
-	const [user, setUser] = useState(null);
-	const id = localStorage.getItem("id")
-
-	useEffect(() => {
-		const fetchUser = async () => {
-			try {
-				const data = await getUser(id);
-				setUser(Array.isArray(data) ? data[0] : data);
-				const userTemp = ((Array.isArray(data) ? data[0] : data))
-				setReserva(userTemp.reservas || [])
-			} catch (error) {
-				console.error("Error cargando usuario:", error);
-			}
-		};
-		fetchUser();
-	}, [id]);
-
-
-
-
-
-
-	console.log(user)
-	console.log(pista)
-
 	
 
 	return (
 		<div className="container">
-
+			
 			<div className="text-center">
-				<h1 className="text-black ">MIS RESERVAS</h1>
+			<h1 className="text-black ">MIS RESERVAS</h1>
 			</div>
 			<div className="row  d-flex justify-content-around">
+				<div className="col-md-3 col-12">
+					<div className="card mi-caja-reservas mt-2 " style={{ width: "18rem;" }}>
+						<img src="/src/front/assets/imagenERROR.jpg" className="card-img-top boorder border-bottom" alt="..." />
+						<div className="card-body">
+							<h5 className="card-title">Nombre club</h5>
+							<p className="card-text">Fecha de reserva</p>
+							<p className="card-text">Horario</p>
 
-				{!user ? (
-					<p className="text-center">Cargando reservas...</p>
-				) : !user.reservas || user.reservas.length === 0 ? (
-					<p className="text-center">No tienes reservas todavía.</p>
-				) : (user.reservas.map((reserva) => (
-					<div key={reserva.id} className="col-md-3 col-12 mt-2">
-						<div className="card mi-caja-reservas" style={{ width: "18rem;" }}>
-							<img src="/src/front/assets/imagenERROR.jpg" className="card-img-top boorder border-bottom" alt="..." />
-							<div className="card-body">
-								<h5 className="card-title">Reserva en {}</h5>
-								<p className="card-text">Fecha de reserva: {reserva.fecha_reserva}</p>
-								<p className="card-text">Horario: {reserva.hora_inicio} a {reserva.hora_fin}</p>
+						</div>
+						<div className="card-body d-flex justify-content-center">
+							<a href="#" className="btn btn-primary">VER MAS</a>
 
-							</div>
-							<div className="card-body d-flex justify-content-center">
-								<Link to={`/reservasInfo/${reserva.id}`}>
-									<a href="#" className="btn btn-primary">VER MAS</a>
-								</Link>
-							</div>
 						</div>
 					</div>
-				))
-				)}
+				</div>
+				<div className="col-md-3 col-12 mt-2">
+					<div className="card mi-caja-reservas" style={{ width: "18rem;" }}>
+						<img src="/src/front/assets/imagenERROR.jpg" className="card-img-top boorder border-bottom" alt="..." />
+						<div className="card-body">
+							<h5 className="card-title">Nombre club</h5>
+							<p className="card-text">Fecha de reserva</p>
+							<p className="card-text">Horario</p>
 
+						</div>
+						<div className="card-body d-flex justify-content-center">
+							<Link to={`/reservasInfo/${12}`}>
+							<a href="#" className="btn btn-primary">VER MAS</a>
+							</Link>
+						</div>
+					</div>
+				</div>
+				<div className="col-md-3 col-12 mt-2">
+					<div className="card mi-caja-reservas" style={{ width: "18rem;" }}>
+						<img src="/src/front/assets/imagenERROR.jpg" className="card-img-top boorder border-bottom" alt="..." />
+						<div className="card-body">
+							<h5 className="card-title">Nombre club</h5>
+							<p className="card-text">Fecha de reserva</p>
+							<p className="card-text">Horario</p>
+
+						</div>
+						<div className="card-body d-flex justify-content-center">
+							<a href="#" className="btn btn-primary">VER MAS</a>
+
+						</div>
+					</div>
+				</div>
+				<div className="col-md-3 col-12 mt-2">
+					<div className="card mi-caja-reservas" style={{ width: "18rem;" }}>
+						<img src="/src/front/assets/imagenERROR.jpg" className="card-img-top boorder border-bottom" alt="..." />
+						<div className="card-body">
+							<h5 className="card-title">Nombre club</h5>
+							<p className="card-text">Fecha de reserva</p>
+							<p className="card-text">Horario</p>
+
+						</div>
+						<div className="card-body d-flex justify-content-center">
+							<a href="#" className="btn btn-primary">VER MAS</a>
+
+						</div>
+					</div>
+				</div>
+
+				<div className="col-md-3 col-12 mt-2">
+					<div className="card mi-caja-reservas" style={{ width: "18rem;" }}>
+						<img src="/src/front/assets/imagenERROR.jpg" className="card-img-top boorder border-bottom" alt="..." />
+						<div className="card-body">
+							<h5 className="card-title">Nombre club</h5>
+							<p className="card-text">Fecha de reserva</p>
+							<p className="card-text">Horario</p>
+
+						</div>
+						<div className="card-body d-flex justify-content-center">
+							<a href="#" className="btn btn-primary">VER MAS</a>
+
+						</div>
+					</div>
+				</div>
 			</div>
-		</div >
+		</div>
 	);
 };              
