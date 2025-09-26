e1c34c06cc39f318d1b0b2fc32cf06390dde6beb
Carlos Enrique Contreras Duque <carlos.contredu@gmail.com>
2025-09-26 12:32:16 +0000

    vuelta a la normalidad el componente pistas, creado componente PistaClub, modificado componente routes,añadidas cosas en el componente CrearPista

--- src/front/pages/CrearPista.jsx
+++ src/front/pages/CrearPista.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { PageHeader } from '../components/PageHeader'
 
 export const CrearPista = () => {
   return (
     <div>
-      <h1>Estoy en proceso de mi formulario tenme un poco de paciencia.</h1>
+      <PageHeader
+        title=""
+      
+      />
+      <h1 className='mb-5'>Estoy en proceso de mi formulario tenme un poco de paciencia "PROFESORES".</h1>
+      <div className="container m-2">
+        <div className="row">
+          <form>
+            <div className="imagen">
+              <label className='mb-1' htmlFor='imagen'>Inserte enlace de la imagen.</label>
+              <input className='form-control mb-3' type='text' id='imagen' placeholder='Obligatoria'required></input>
+            </div>
+            <div className="imagenDos">
+              <label className='mb-1' htmlFor='imagenDos'>Inserte segunda imagen.</label>
+              <input className='form-control mb-3' type='text' id='imagenDos' placeholder='Opcional'></input>
+            </div>
+            <div className="imagenTres">
+              <label className='mb-1' htmlFor='imagenTres'>Inserte tercera imagen.</label>
+              <input className='form-control mb-3' type='text' id='imagenTres' placeholder='Opcional'></input>
+            </div>
+            <div className='Numero de pista'>
+              <label className='mb-1' htmlFor='numeropista'>Numero de la pista.</label>
+              <input className='form-control mb-3' type='text' id='numeropista' placeholder='indica el numero de la pista' required></input> 
+            </div>
+
+          </form>
+
+        </div>
+      </div>
     </div>
   )
 }
--- src/front/pages/Pistas.jsx
+++ src/front/pages/Pistas.jsx
@@ -1,68 +1,53 @@
 import { useEffect, useState } from "react";
-import { getPistas, getPistasClub, getReservas } from "../../services/servicesAPI.js";
-import { useNavigate, Link, useParams } from "react-router-dom";
-import { PageHeader } from "../components/PageHeader";
+import { getPistas, getReservas } from "../../services/servicesAPI.js";
+import { useNavigate, Link } from "react-router-dom";
 
 
 export const Pistas = () => {
 
-	const navigate = useNavigate()
-	const {id} = useParams()
-	const [pistas, setPistas] = useState([]);
-	console.log(id);
-	
-	useEffect(() => {
-		if (localStorage.getItem("token") == null) {
-			navigate("/")
-		} else {
-			getPistasClub(id).then(
-				data => {
-					if (Array.isArray(data) && data.length > 0) {
-						setPistas(data)
-					}
-				}
-			)
-		}
-	}, [localStorage.getItem("token")])
+    const navigate = useNavigate()
 
-	return (
+    const [pistas, setPistas] = useState([]);
 
-		<>
-			<PageHeader
-				title="Puedes ver todas nuestras pistas"
-				lead="Aqui encontraras todas las pistas de las que disponemos para ti y tus acompañantes, disfruta del encanto de nuestro club."
-			/>
-			
-			<button type="button" onClick={()=>navigate("/crearPista")} class="btn btn-primary">
-				Crear pista
-			</button>
+    useEffect(() => {
+        if (localStorage.getItem("token") == null) {
+            navigate("/")
+        } else {
+            getPistas().then(
+                data => {
+                    if (Array.isArray(data) && data.length > 0) {
+                        setPistas(data)
+                    }
+                }
+            )
+        }
+    }, [localStorage.getItem("token")])
 
-			<div className="container row d-flex justify-content-center gap-3">
-				{pistas?.map(pista => (
+    return (
+        <div className="container d-flex justify-content-center gap-3">
+            {pistas?.map(pista => (
 
-					<div className="card mt-3" key={pista.id} style={{ width: "80rem" }}>
-						{/* <img src="..." className="card-img-top" alt="..." /> */}
-						<div className="card-body">
-							<div className="d-flex justify-content-between ">
-								<h4 className="card-title">Pista {pista.numero_pista}</h4>
-								<h4 className="card-title" style={{ color: "#3374beff" }}>{pista.club_info['nombre']}</h4>
-							</div>
-							<p className="card-text mb-4">
-								Superficie: {pista.superficie === 'cesped'
-									? "césped"
-									: pista.superficie === 'hormigon'
-										? "hormigón"
-										: "sintético"}
-							</p>
-							<Link to={`/calendario-pista/${pista.id}`}>
-								<button href="#" className="btn btn-primary boton-padelplus">Consultar horarios disponibles</button>
-							</Link>
-						</div>
-					</div>
+                <div className="card mt-3" key={pista.id} style={{ width: "800px" }}>
+                    {/* <img src="..." className="card-img-top" alt="..." /> */}
+                    <div className="card-body">
+                        <div className="d-flex justify-content-between ">
+                            <h4 className="card-title">Pista {pista.numero_pista}</h4>
+                            <h4 className="card-title text-end" style={{ color: "#3374beff" }}>{pista.club_info['nombre']}</h4>
+                        </div>
+                        <p className="card-text mb-4">
+                            Superficie: {pista.superficie === 'cesped'
+                                ? "césped"
+                                : pista.superficie === 'hormigon'
+                                    ? "hormigón"
+                                    : "sintético"}
+                        </p>
+                        <Link to={`/calendario-pista/${pista.id}`}>
+                            <button href="#" className="btn btn-primary boton-padelplus">Consultar horarios disponibles</button>
+                        </Link>
+                    </div>
+                </div>
 
-				))}
-			</div>
-		</>
-
-	);
+            ))}
+        </div>
+    );
 };
