3bb104680a3331aeda35257904efd61ac5e568f9
Carlos Enrique Contreras Duque <carlos.contredu@gmail.com>
2025-09-30 10:27:24 +0000

    formulario cambiado

--- src/front/pages/CrearPista.jsx
+++ src/front/pages/CrearPista.jsx
@@ -2,14 +2,23 @@ import React, { useState } from 'react';
 import { Navigate } from 'react-big-calendar';
 import { PageHeader } from "../components/PageHeader";
 import { file } from 'zod';
+import { createPista } from '../../services/servicesAPI';
 
 export const CrearPista = () => {
 
   
   const [numeropista, setnumeroPista] = useState ("");
-  const [imagen, setImagen] = useState (file[0]);
-  const [imagenpistaDos, setImagenpistaDos] = useState (file[0]);
-  const [imagenpistaTres, setImagenpistaTres] = useState (file[0]);
+  const [preciohora, setprecioHora] = useState ("");
+  const [superficie, setSuperficie] = useState ("");
+  const [estado, setEstado] = useState ("");
+
+  const createPistaForApi = async ()=>{
+    const pistaClub = await createPista();
+    setPistas(pistaClub);
+
+  }
+
+
   return (
     <>
 
@@ -22,13 +31,48 @@ export const CrearPista = () => {
         <div className='titulo p-3'>
           <h1 className=''>Como Club aqui tienes el proceso para poder añadir tantas pistas como tengas.</h1>
         </div>
-        <form className='p-3'>
+        <form className='p-3'onSubmit={createPistaForApi}>
           <label className='mb-3 mt-3' htmlFor='numero de pista'>Introduce el numero de pista</label>
-          <input type='text' className='form-control mb-5' id='numero de pista' placeholder='introduce el numero de la pista' required></input>
-          <label className='mb-3' htmlFor='imagenes'>Seleciona las imagenes de tu pista</label>
-          <input type="file" className='imagen form-control mb-2' required />
-          <input type="file" className='imagenpistaDos form-control mb-2' />
-          <input type="file" className='imagenpistaTres form-control mb-2' />
+          <input type='text' 
+          className='form-control mb-5' 
+          id='numero de pista' 
+          placeholder='introduce el numero de la pista' 
+          value={numeropista}
+          onChange={e.target.value}
+          required
+          />
+          <label className='form-label mb-3' htmlFor='superficie'>Seleciona tipo de superficie</label>
+          <input className='form-control mb-2'
+          list='optionSuperficie' 
+          id='superficie' 
+          placeholder='tipo de superficie' 
+          value={superficie}
+          onChange={e.target.value}
+          required />
+          <datalist id='optionSuperficie'>
+            <option value="Cesped."/>
+            <option value="Hormigon."/>
+            <option value="Sintetico."/>
+          </datalist>
+          <label className='form-label mb-3 ' htmlFor='precio pista'>Precio Hora.</label>
+          <input type="text" 
+          className='form-control mb-2' 
+          id='precio pista'
+          value={preciohora}
+          onChange={e.target.value}
+          />
+          <label className='form-label mb-3' htmlFor='estado'>Seleciona estado de la pista</label>
+          <input className='form-control mb-2' 
+          list='optionEstado' 
+          id='estado' 
+          placeholder='Estado de la pista' 
+          value={estado}
+          onChange={e.target.value}
+          required />
+          <datalist id='optionEstado'>
+            <option value="Libre."/>
+            <option value="Mantenimiento."/>
+          </datalist>
           <div className='d-flex justify-content-center align-items-center'>
             <button className='btn btn-outline-primary mt-3' type='submit' >Guardar pista.</button>
           </div>
@@ -40,4 +84,4 @@ export const CrearPista = () => {
 
 // RECORDATORIO IMPORTANTE:
 
-// EN EL BACKEND NECESITAMOS LO QUE ES PARA LA PISTA TRES OPCIONES DE IMAGEN QUE SE PUEDAN REGISTRAR Y SE PUEDAN GUARDAR
+// YA NO SE PUEDE TOCAR BACKEND, AL NO TENER LA OPCION DE IMAGEN LA CARTA SE QUEDARA SIMPLE CON SOLO TEXT.
